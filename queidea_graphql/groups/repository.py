74b46036b4d8c7035d58bb49d4e6da8541cbd1b2
Mario Garcia <mario.garcia@kaleidos.net>
2019-08-25 02:15:26 +0200

    chores (persistence) add try/exceptr when saving objects

--- queidea_graphql/groups/repository.py
+++ queidea_graphql/groups/repository.py
@@ -1,5 +1,6 @@
 from queidea_graphql.config.logger import log
 from queidea_graphql.config.db import db_session
+from queidea_graphql.config.uuid import generate_id
 from .models import Group
 
 
@@ -9,9 +10,18 @@ def save(name):
     as argument
     """
     log.msg("group/create", name=name)
-    group_to_save = Group(name=name)
+    group_uuid = generate_id()
+    group_to_save = Group(
+        uuid=group_uuid,
+        name=name
+    )
 
-    db_session.add(group_to_save)
-    db_session.commit()
+    try:
+        db_session.add(group_to_save)
+        db_session.commit()
+        log.msg("group/create/success", uuid=group_uuid)
+    except:
+        log.msg("group/create/failure")
+        db_session.rollback()
 
     return group_to_save
--- queidea_graphql/links/repository.py
+++ queidea_graphql/links/repository.py
@@ -1,5 +1,6 @@
 from queidea_graphql.config.logger import log
 from queidea_graphql.config.db import db_session
+from queidea_graphql.config.uuid import generate_id
 from .models import Link
 
 
@@ -8,9 +9,19 @@ def save(group_id, description, uri):
     Saves a new link
     """
     log.msg("link/create", group_id=group_id, uri=uri)
-    link_to_save = Link(group_id=group_id, description=description, uri=uri)
+    link_uuid = generate_id()
+    link_to_save = Link(
+        uuid=link_uuid,
+        group_id=group_id,
+        description=description, uri=uri
+    )
 
-    db_session.add(link_to_save)
-    db_session.commit()
+    try:
+        db_session.add(link_to_save)
+        db_session.commit()
+        log.msg("link/create/success", uuid=link_uuid)
+    except:
+        log.msg("link/create/failure")
+        db_session.rollback()
 
     return link_to_save
