30fa63e3adeadc2edac60a72bdb1210f575546a3
Mario Garcia <mario.garcia@kaleidos.net>
2020-01-22 23:11:44 +0100

    fix (cli) add cli commands

--- manage.py
+++ manage.py
@@ -1,7 +1,14 @@
+import click
 from queidea_graphql import app
 from queidea_fixtures import fixtures
 
 
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 def runserver():
     """
     Starts up queidea GraphQL api (development)
@@ -9,12 +16,21 @@ def runserver():
     app.runapp()
 
 
+@cli.command()
 def create_schema():
+    """
+    Creates model schema in the database
+    """
     fixtures.create_schema()
 
 
+@cli.command()
 def add_fixtures():
     """
     Adds fixtures data to app
     """
     fixtures.create_data()
+
+
+if __name__ == '__main__':
+    cli()
